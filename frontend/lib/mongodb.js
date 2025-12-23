544549765e10c12c14518b5e3fb45a9713c189d1
haste13 <hastemuhsin2005@gmail.com>
2025-12-23 22:23:20 +0300

    Add MongoDB connection logging

--- frontend/lib/mongodb.js
+++ frontend/lib/mongodb.js
@@ -37,6 +37,7 @@ async function connectDB() {
         cached.conn = await cached.promise;
     } catch (e) {
         cached.promise = null;
+        console.error('MongoDB connection error:', e);
         throw e;
     }
 
